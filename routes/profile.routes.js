99c7d149294f8b5b6645bba32fc8b587a474da48
Sandra Mendez <scmendez3b@gmail.com>
2020-11-11 17:12:28 +0100

    looking at edit profile refresh bug

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -3,6 +3,22 @@ const router  = express.Router();
 
 const ClimberModel = require('../models/Climber.model')
 
+router.get('/climberInfo/:climberId', (req, res) => {
+  let climberId = req.params.climberId
+
+  ClimberModel.findById(climberId)
+    .then((climberInfo) => {
+      console.log('climberinfopls', climberInfo)
+      res.status(200).json(climberInfo)
+    })
+    .catch((err) => {
+      res.status(500).json({
+      error: 'could not populate all projects',
+      message: err
+    })
+  })
+
+})
 
 router.patch('/editUsername/:climberId', (req, res, next) => {
   let climberId = req.params.climberId
@@ -10,8 +26,13 @@ router.patch('/editUsername/:climberId', (req, res, next) => {
   const { username } = req.body;
 
   ClimberModel.findByIdAndUpdate(climberId, {$set: {username: username}})
-    .then((response) => {
-      res.status(200).json(response)
+    .then(() => {
+      ClimberModel.findById(climberId)
+        .then((responseAgain) => {
+          console.log(responseAgain)
+          res.status(200).json(responseAgain)
+        })
+      
     })
     .catch((err) => {
           console.log(err)
